d879de0f8fac7fc2dd008f842173d6d61f47f489
lorexgmd <andrejbarysnikov497@gmail.com>
2025-04-15 11:04:20 +0200

    added json and logica for it to work

---
+++ nuxt-app/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>JSON data</h1>
+    <pre>{{ jsonData }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const jsonData = ref([])
+
+onMounted(async () => {
+  const res = await fetch('/data.json')
+  jsonData.value = await res.json()
+})
+</script>
